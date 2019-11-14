54b01ab90e2bd77c85d8b8c5146206c309b7cbd2
Tyrone <tyronesaldanha@gmail.com>
2019-11-14 19:35:34 +0530

    issue with logging output expression in 'else if'

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 var input=document.getElementById("input");
 var result=document.getElementById("result");
-var key=document.getElementsByClassName("button_section").addEventListener('click',keyPress);
-
+var key=document.querySelector('.button_section').addEventListener('click',keyPress)
+var operators=['+','-','/','AC','C','X','%','='];
+var numbers=['0','1','2','3','4','5','6','7','8','9'];
+input.value="";
 function keyPress(event)
 {
     //Do nothing as field empty and any operators or element other than button is touched
-    if(['+','-','/','AC','C','X','%','='].includes(event.target.innerHTML)&&input.value==="" || event.target.tagName!='BUTTON')
-    {return;}
+    if(operators.includes(event.target.innerHTML)&&input.value==="" || event.target.tagName!='BUTTON'||"AC"===event.target.innerHTML)
+    {input.value="";}
 
-    //Number key press
-    //if(){}
+    //back to back Number key press
+    if(numbers.includes(event.target.innerHTML)&&input.value===""||numbers.includes(event.target.innerHTML)&&input.value!='')
+    {
+        //console.log(event.target.innerHTML);
+        input.value=input.value+event.target.innerHTML;
+    }
+    //Number and operator key press
+    else if(input.value!=''&&operators.includes(event.target.innerHTML)&&"AC"!=event.target.innerHTML&&"="!=event.target.innerHTML)
+    {
+        input.value=input.value+event.target.innerHTML;
+        console.log(input.value)
+        while(numbers.includes(input.value[input.value.length - 1]) && operators.includes(input.value[input.value.length - 2]))
+        {
+            //result.innerHTML=eval('input.value');
+            console.log('aint printing');
+        }
+    }
+    else (console.log("some issue in code"))
 }
