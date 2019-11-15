2cb50a5c4a81a9fa8363217cb567f7c918ae88b5
Tyrone <tyronesaldanha@gmail.com>
2019-11-15 19:22:28 +0530

    everything working except '%' so far

--- script.js
+++ script.js
@@ -2,30 +2,58 @@ var input=document.getElementById("input");
 var result=document.getElementById("result");
 var key=document.querySelector('.button_section').addEventListener('click',keyPress)
 var operators=['+','-','/','AC','C','X','%','='];
-var numbers=['0','1','2','3','4','5','6','7','8','9'];
-input.value="";
+var numbers=['0','1','2','3','4','5','6','7','8','9','.'];
+
 function keyPress(event)
 {
+    var btnPress=event.target.innerHTML;
     //Do nothing as field empty and any operators or element other than button is touched
-    if(operators.includes(event.target.innerHTML)&&input.value==="" || event.target.tagName!='BUTTON'||"AC"===event.target.innerHTML)
-    {input.value="";}
-
-    //back to back Number key press
-    if(numbers.includes(event.target.innerHTML)&&input.value===""||numbers.includes(event.target.innerHTML)&&input.value!='')
+    if(['/','X','+'].includes(btnPress)&&input.value==="" || event.target.tagName!='BUTTON'||"AC"===btnPress)
     {
-        //console.log(event.target.innerHTML);
-        input.value=input.value+event.target.innerHTML;
+        input.value="";
+        result.innerHTML="";
     }
-    //Number and operator key press
-    else if(input.value!=''&&operators.includes(event.target.innerHTML)&&"AC"!=event.target.innerHTML&&"="!=event.target.innerHTML)
+
+    if(btnPress != 'C' && btnPress != '=' && btnPress != 'AC')
     {
-        input.value=input.value+event.target.innerHTML;
-        console.log(input.value)
-        while(numbers.includes(input.value[input.value.length - 1]) && operators.includes(input.value[input.value.length - 2]))
+        if(operators.includes(btnPress) && operators.includes(input.value[input.value.length - 1]))
+        {//Changes operator to another if no number after
+            input.value = input.value.substring(0, input.value.length - 1) + btnPress;
+        }
+        else
         {
-            //result.innerHTML=eval('input.value');
-            console.log('aint printing');
+            input.value=input.value+btnPress;
+        }
+        eval_expr();
+    }
+    else if(btnPress==="C")
+    {//backspacing input
+        input.value=input.value.substring(0,input.value.length - 1);
+        eval_expr();
+    }
+    else if(btnPress==='=')
+    {
+        input.value=result.innerHTML;
+        result.innerHTML="";
+    }
+}
+
+function eval_expr()
+{
+    try
+    {//regular expression used as eval has issue with X as multiply 
+        result.innerHTML = eval(input.value.replace(/X/g,'*'));
+        if(result.innerHTML==input.value)
+        {//blanks out result field if u type a number and press =
+            result.innerHTML="";
         }
     }
-    else (console.log("some issue in code"))
+    catch(err)
+    {
+        if(eval(input.value.substring(0, input.value.length - 1).replace(/X/g,'*')) == input.value.substring(0, input.value.length - 1))
+        {
+            result.innerHTML = "";
+        }
+    }
+    input.focus();
 }
